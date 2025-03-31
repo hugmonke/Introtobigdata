9d9eb6fe5d3c8782af594dae31cddd471f138ee4
Filip H <107533273+hugmonke@users.noreply.github.com>
2025-03-31 17:42:14 +0200

    Update test_classifier.py

--- test_classifier.py
+++ test_classifier.py
@@ -1,46 +1,29 @@
 import torch
 import torchvision
 from torchvision.transforms import v2
-import torch.utils.data as data
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+from torch.utils.data import Subset, DataLoader
+
 import os
-import matplotlib.pyplot as plt
-import numpy as np
-from PIL import ImageFile
 import time
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import ImageFile
 
-class CNN(nn.Module):
+from cnnetwork import CNN
 
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2) 
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 56 * 56, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 9)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        return x
 
 def imshow(img):
+    # Use imshow(torchvision.utils.make_grid(images)) to show an image grid
+    # Use print(' '.join(f'{classes[labels[j]]:5s}' for j in range(BATCH_SIZE))) to print their respective classes
     img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-if __name__ == '__main__':
-    
+def main():
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+    LOAD_PATH = './Models/model{}.pth'.format(int(time.time()))
     KAGGLE = "Kaggle"
     BATCH_SIZE = 10
 
@@ -53,23 +36,28 @@ if __name__ == '__main__':
     ])
 
     mushroom_jpegs = torchvision.datasets.ImageFolder(root=KAGGLE, transform=transforms)
-    n = len(mushroom_jpegs)  
-    n_test = int(0.2 * n)  
-    test_set = data.Subset(mushroom_jpegs, range(n_test))
-    test_set_loader  = data.DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=0) 
 
-    classes = next(os.walk(KAGGLE))[1] # Lists folder names - ergo lists mushroom family
-    
-    # print(classes)
+    jpeg_amount = len(mushroom_jpegs)  
+    test_jpeg_amount = int(0.2 * jpeg_amount)  
+    test_set = Subset(mushroom_jpegs, range(test_jpeg_amount))
+    train_set = Subset(mushroom_jpegs, range(test_jpeg_amount, jpeg_amount))
+
+    test_set_loader  = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=0) 
+    train_set_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True,  num_workers=0)
+
+    classes = next(os.walk(KAGGLE))[1]
+    print(classes)
 
     dataiter = iter(test_set_loader)
     images, labels = next(dataiter)
-    
-    # imshow(torchvision.utils.make_grid(images))
+    imshow(torchvision.utils.make_grid(images))
     print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(BATCH_SIZE)))
+
+
     net = CNN()
     try:
-        LOAD_PATH = './Models/model{}.pth'.format(int(time.time()))
+        net.load_state_dict(torch.load(LOAD_PATH, weights_only=True))
+        print("Given load path is incorrect. Trying latest file.")
     except:
         try:
             import glob
@@ -94,7 +82,8 @@ if __name__ == '__main__':
             match += 1
     print("Match:", match, "out of ", len(one), " RATIO: ", match/len(one))
 
-
+if __name__ == '__main__':
+    main()
 
     # #path = os.path.join(KAGGLE, fungi_family[0])
     # #directory = os.fsencode(path)
