26d6e6c00723a1ddaa8156ebe848b50faba59a93
Filip H <107533273+hugmonke@users.noreply.github.com>
2025-04-01 14:42:47 +0200

    Update cnnetwork.py

--- cnnetwork.py
+++ cnnetwork.py
@@ -7,12 +7,12 @@ class CNN(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=8, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2) 
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 56 * 56, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 9)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(16 * 198 * 248, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
